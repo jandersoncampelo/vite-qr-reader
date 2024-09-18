bac1cdbc9857588418f9021e2f4e509e30d4c506
BECOMEX\janderson.souza <janderson.souza@becomex.com.br>
2024-09-18 20:03:04 -0300

    adicionado estilos

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import QRCodeReader from "./components/QRCodeReader";
 import { useState } from "react";
 
 function App() {
-  const [scanResult, setScanResult] = useState<string | null>(null);
+  const [scanResult, setScanResult] = useState<string | null>('http://www.fazenda.pr.gov.br/nfce/qrcode?p=41240902168202001144652090003353741000058867|2|1|1|66A8C636CF37A01F46D9635AD16D6A5893165496');
 
   const onNewScanResult = (decodedText: string, decodedResult: unknown) => {
     console.log(decodedText, decodedResult);
@@ -12,22 +12,20 @@ function App() {
 
   return (
     <>
-      <QRCodeReader
-        fps={10}
-        qrbox={250}
-        disableFlip={false}
-        qrCodeSuccessCallback={onNewScanResult}
-      />
-      <div className="App">
-        <header className="App-header">
+        <header className="app-header">
           <p>QR Code Scanner</p>
           <div id="qr-code-result" />
         </header>
-      </div>
+        <QRCodeReader
+        fps={10}
+        qrbox={{ width: 250, height: 250 }}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
 
       {scanResult && (
         <div className="scan-result">
-          <p>Scan Result:</p>
+          <h2>Scan Result:</h2>
           <p>{scanResult}</p>
         </div>
       )}
