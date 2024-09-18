9de7b1119fb16a497fc16a6789731f04e1b40060
BECOMEX\janderson.souza <janderson.souza@becomex.com.br>
2024-09-17 21:59:30 -0300

    Add result

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import './App.css'
-import QRCodeReader from './components/QRCodeReader'
+import "./App.css";
+import QRCodeReader from "./components/QRCodeReader";
+import { useState } from "react";
 
 function App() {
+  const [scanResult, setScanResult] = useState<string | null>(null);
+
   const onNewScanResult = (decodedText: string, decodedResult: unknown) => {
-    console.log(decodedText, decodedResult)
-};
+    console.log(decodedText, decodedResult);
+    setScanResult(decodedText);
+  };
 
   return (
-    <QRCodeReader
-      fps={10}
-      qrbox={250}
-      disableFlip={false}
-      qrCodeSuccessCallback={onNewScanResult}
-    />
-  )
-}
+    <>
+      <QRCodeReader
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
+      <div className="App">
+        <header className="App-header">
+          <p>QR Code Scanner</p>
+          <div id="qr-code-result" />
+        </header>
+      </div>
 
-export default App
+      {scanResult && (
+        <div className="scan-result">
+          <p>Scan Result:</p>
+          <p>{scanResult}</p>
+        </div>
+      )}
+    </>
+  );
+}
+export default App;
